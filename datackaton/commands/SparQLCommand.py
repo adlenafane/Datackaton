024e1c64b6309fddd3d076855512ea9b83b0779c
Antoine Durieux <adurieux@stanford.edu>
2013-02-02 15:48:28 +0100

    Enhanced command.

--- datackaton/commands/SparQLCommand.py
+++ datackaton/commands/SparQLCommand.py
@@ -24,32 +24,73 @@ class SparQLCommand(BaseCommand):
         http://sparql-wrapper.sourceforge.net/
         
         Example use :
-        python datackaton/commands/SparQLCommand.py
+        python datackaton/commands/SparQLCommand.py --regions
     """
     
     option_list = BaseCommand.option_list + (
-        make_option('--show-errors', action='store', dest='show-errors', default=False, help='Prints the bad results', type="int"),
+        make_option('--region', action='store', dest='region', default=False, help='Loads the list of regions', type="int"),
+        make_option('--cepage', action='store', dest='cepage', default=False, help='Loads the list of cepages', type="int"),
     )
     
     def handle(self, *args, **options):
+        """
+            Configures the SparQL endpoint to tap into fr.dbpedia.
+            Routes the request to the appropriate function.
+        """
     
-        sparql = SPARQLWrapper("http://fr.dbpedia.org/sparql")
-        sparql.setQuery(u"""
+        self.sparql = SPARQLWrapper("http://fr.dbpedia.org/sparql")
+        self.sparql.setReturnFormat(JSON)
+        
+        print(options)
+        
+        if options['region']:
+            self.load_region()
+        
+        if options['cepage']:
+            self.load_cepage()
+        
+        
+    def load_cepage(self):
+        """
+            Retrieves the cepages based on their template.
+        """
+        
+        self.sparql.setQuery(u"""
             PREFIX dbo: <http://dbpedia.org/ontology/> 
             PREFIX res: <http://dbpedia.org/resource/> 
             PREFIX dbp: <http://fr.dbpedia.org/property/> 
-            SELECT ?appelation ?cepages
+            SELECT ?uri
             WHERE { 
-                ?appelation prop-fr:wikiPageUsesTemplate <http://fr.dbpedia.org/resource/Modèle:Infobox_Région_viticole> .
-                ?appelation prop-fr:cépages ?cepages
+                ?uri prop-fr:wikiPageUsesTemplate <http://fr.dbpedia.org/resource/Modèle:Infobox_Cépage> .
             }
         """)
-        sparql.setReturnFormat(JSON)
         
-        results = sparql.query().convert()
+        results = self.sparql.query().convert()
         for result in results["results"]["bindings"]:
-            print(result["appelation"]["value"], result["cepages"]["value"])
-    
+            print(result["uri"]["value"])
+        print len(results)
+        
+        
+        
+    def load_region(self):
+        """
+            Retrieves the regions based on their template.
+        """
+        
+        self.sparql.setQuery(u"""
+            PREFIX dbo: <http://dbpedia.org/ontology/> 
+            PREFIX res: <http://dbpedia.org/resource/> 
+            PREFIX dbp: <http://fr.dbpedia.org/property/> 
+            SELECT ?uri
+            WHERE { 
+                ?uri prop-fr:wikiPageUsesTemplate <http://fr.dbpedia.org/resource/Modèle:Infobox_Région_viticole> .
+            }
+        """)
+        
+        results = self.sparql.query().convert()
+        for result in results["results"]["bindings"]:
+            print(result["uri"]["value"])
+        print len(results)
     
     
 if __name__== "__main__" :
