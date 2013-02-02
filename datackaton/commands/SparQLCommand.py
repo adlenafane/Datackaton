5107285d59aa0f5f440e296d86b512b37344faeb
adlenafane <adlen.afane@gmail.com>
2013-02-02 12:42:40 +0100

    added a try/except to avoid unicode issues
    issues in windows console

--- datackaton/commands/SparQLCommand.py
+++ datackaton/commands/SparQLCommand.py
@@ -44,8 +44,10 @@ class SparQLCommand(BaseCommand):
         results = sparql.query().convert()
 
         for result in results["results"]["bindings"]:
-            print(result["label"]["value"])
-
+            try:
+                print(result["label"]["value"])
+            except:
+                pass
     
     
     
